55831ac4300cd0781bc055e626821cec4081e828
Maulana Satya Adi <maulanasatyaadi@gmail.com>
2020-10-02 16:37:20 +0700

    Add mongoose connection

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,20 @@ import mongoose from 'mongoose'
 import process from 'process'
 
 const PORT = process.env.PORT || 3000
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/appdb'
+
+mongoose
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.log('Failed when connecting to MongoDB')
+    console.warn(err)
+  })
 
 let server = express()
 
